442e608586c5709c2795ab64c5709966bbf67124
Kacper <kacper-sleziak@wp.pl>
2022-03-08 21:44:29 +0100

    Wrong path to urlpattern fixed

--- backend/Polling_App/urls.py
+++ backend/Polling_App/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('Account.api.urls')),
-    path('polls/', include('Polls.api.urls')),
+    path('account/', include('Account.API.urls')),
+    path('polls/', include('Polls.API.urls')),
 ]
