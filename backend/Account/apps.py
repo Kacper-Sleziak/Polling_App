f93032ff92e9e19acdf15d8d1715aef22a4aafbf
Kacper <kacper-sleziak@wp.pl>
2022-03-08 21:38:10 +0100

    Signals Setup
    + Created Signals files
    + Added Account and Polls app to main settings

--- backend/Account/apps.py
+++ backend/Account/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class AccountConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'Account'
+    
+    def ready(self):
+        import Account.signals
--- backend/Polling_App/settings.py
+++ backend/Polling_App/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    
+    #Project Apps 
+    'Account',
+    'Polls'
 ]
 
 MIDDLEWARE = [
