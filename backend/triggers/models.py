e6f02d1095083952927a412e14fa4f47f008eabb
Kacper <kacper-sleziak@wp.pl>
2022-03-14 19:25:53 +0100

    Created Trigger Model

--- backend/triggers/models.py
+++ backend/triggers/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from questions.models import Question
 
 class Trigger(models.Model):
-    pass
+    triggering_answer = models.CharField(verbose_name="triggering answer", max_length=200)
+    triggering_question = models.ForeignKey(Question, related_name ='%(class)s_triggering', on_delete=models.CASCADE)
+    triggered_question = models.ForeignKey(Question, related_name ='%(class)s_triggered', on_delete=models.CASCADE)
