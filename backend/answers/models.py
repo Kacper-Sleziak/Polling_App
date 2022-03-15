93ed4284f452284f7787e64b29118b8995ce9795
Bodzioss <marcin_bogdanski1@wp.pl>
2022-03-15 15:57:31 +0100

    Created Answer and Answers models

--- backend/answers/models.py
+++ backend/answers/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
-class Answer(models.Model):
-    pass
-
 class Answers(models.Model):
-    pass
+    question_ID = models.ForeignKey(verbose_name="question ID", on_delete=models.CASCADE)
+
+class Answer(models.Model):
+    answers_ID = models.ForeignKey(verbose_name="answers ID", on_delete=models.CASCADE)
+    text_answer = models.CharField(verbose_name="text answer", max_length=2000)
+    option_ID = models.ForeignKey(verbose_name="option ID", on_delete=models.CASCADE)
