0e94cf7625359289dac5b54b7b05a1231f52dcd9
Bodzioss <marcin_bogdanski1@wp.pl>
2022-03-15 17:40:31 +0100

    Updated answers model

--- backend/answers/models.py
+++ backend/answers/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from questions.models import Question
 
 class Answers(models.Model):
-    question_ID = models.ForeignKey(verbose_name="question ID", on_delete=models.CASCADE)
+    question_ID = models.ForeignKey(Question,verbose_name="question ID", on_delete=models.CASCADE)
 
 class Answer(models.Model):
-    answers_ID = models.ForeignKey(verbose_name="answers ID", on_delete=models.CASCADE)
+    answers_ID = models.ForeignKey(Answers,related_name ='%(class)s_answer', verbose_name="answers ID", on_delete=models.CASCADE)
     text_answer = models.CharField(verbose_name="text answer", max_length=2000)
-    option_ID = models.ForeignKey(verbose_name="option ID", on_delete=models.CASCADE)
+    option_ID = models.ForeignKey(Answers, verbose_name="option ID",related_name ='%(class)s_option', on_delete=models.CASCADE)
