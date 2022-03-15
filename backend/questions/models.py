d65efcfd6bdd80dfdb80d84fc00f780243669dc0
Karol Pastewski <karolpastewski29@gmail.com>
2022-03-15 20:25:40 +0100

    Formatting changes

--- backend/questions/models.py
+++ backend/questions/models.py
@@ -1,15 +1,20 @@
-from tabnanny import verbose
 from django.db import models
 from polls import models as polls_models
+
 
 class QuestionType(models.Model):
     type_name = models.TextField(max_length=2000, default="New question type")
 
+
 class Question(models.Model):
-    content = models.TextField(default="Content" ,max_length=2000)
-    poll_ID = models.ForeignKey(polls_models.Poll, on_delete=models.CASCADE, verbose_name="poll ID")
-    question_ID = models.ForeignKey(QuestionType, on_delete=models.CASCADE, verbose_name="question ID")
+    content = models.TextField(default="Content", max_length=2000)
+    poll_ID = models.ForeignKey(
+        polls_models.Poll, on_delete=models.CASCADE, verbose_name="poll ID")
+    question_ID = models.ForeignKey(
+        QuestionType, on_delete=models.CASCADE, verbose_name="question ID")
+
 
 class Option(models.Model):
-    content = models.TextField(default="Option" ,max_length=2000)
-    question_id = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name="question ID")
+    content = models.TextField(default="Option", max_length=2000)
+    question_id = models.ForeignKey(
+        Question, on_delete=models.CASCADE, verbose_name="question ID")
