4a97324d48c1fc92564927d2353476dccff37cd3
Kacper <kacper-sleziak@wp.pl>
2022-03-18 13:48:53 +0100

    Moved triggers validation logic from api views to models

--- backend/triggers/api/views.py
+++ backend/triggers/api/views.py
@@ -10,22 +10,12 @@ from triggers.api.serializers import TriggerSerializer
 class CreateTriggerView(APIView):
     serializer_class = TriggerSerializer
     
-    def are_questions_from_same_poll(self, triggering_question, triggered_question):
-        if triggering_question.poll == triggered_question.poll:
-            return True
-        return False
-    
     def post(self, request, format=None):
         serializer = self.serializer_class(data=request.data)
         
         if serializer.is_valid():
-            triggering_question = serializer.validated_data['triggering_question']
-            triggered_question = serializer.validated_data['triggered_question']
-            if self.are_questions_from_same_poll(triggering_question, triggered_question):
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            return Response({'error': "Questions come from different polls!"}, 
-                            status=status.HTTP_400_BAD_REQUEST)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
 # [GET, DELETE] Trigger View
