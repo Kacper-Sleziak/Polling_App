cd7e604daff937a7f28f00b2791037af92f5ae6c
Kacper <kacper-sleziak@wp.pl>
2022-03-18 13:45:25 +0100

    Created drf exception handler

--- backend/Polling_App/settings.py
+++ backend/Polling_App/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -60,6 +61,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'EXCEPTION_HANDLER' : 'Polling_App.utils.exception_handler',
+}
 
 CORS_ALLOWED_ORIGINS = [
     # Orgin of frontend app
---
+++ backend/Polling_App/utils.py
@@ -0,0 +1,13 @@
+from django.core.exceptions import ValidationError as DjangoValidationError
+from rest_framework.exceptions import ValidationError as DRFValidationError
+from rest_framework.serializers import as_serializer_error
+from rest_framework.views import exception_handler as drf_exception_handler
+
+
+# Convert standard Django Validation Error to DRF one 
+# Help to keep convention ,,Thick models and thin views"
+def exception_handler(exc, context):
+    if isinstance(exc, DjangoValidationError):
+        exc = DRFValidationError(as_serializer_error(exc))
+
+    return drf_exception_handler(exc, context)
