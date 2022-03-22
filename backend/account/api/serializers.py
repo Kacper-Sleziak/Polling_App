d8d0d4ab7ac69ee23332a7d3f8ca8948306eb250
Kacper Śleziak <kacper-sleziak@wp.pl>
2022-03-22 16:23:41 +0100

    Created Register End Point

--- backend/account/api/serializers.py
+++ backend/account/api/serializers.py
@@ -1,6 +1,7 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, CharField
 from django.core.validators import EmailValidator
 from account.models import Account
+from account.utils import PasswordValidator
 
 
 class LoginSerializer(ModelSerializer):
@@ -17,3 +18,31 @@ class AccountSerializer(ModelSerializer):
         model = Account
         fields = ('email', 'company_name', 'date_joined', 'last_login', 'is_admin',
                   'is_active', 'is_staff', 'is_superuser')
+
+class CreateAccountSerializer(ModelSerializer):
+    
+    password = CharField(style={'input_type': 'password'}, write_only=True)
+    password2 = CharField(style={'input_type': 'password'}, write_only=True)
+    
+    class Meta:
+        model = Account
+        fields = ('company_name', 'email', 'password', 'password2')
+        
+    def validate(self, data):
+        password = data['password']
+        password2 = data['password2']
+        
+        # Using custome validator
+        password_validator = PasswordValidator(password=password)
+        password_validator.validate(length=9, password2=password2)
+        return data
+        
+    def save(self):
+        account = Account(
+            email=self.validated_data['email'],
+            company_name=self.validated_data['company_name']
+        )
+        password = self.validated_data['password']
+        account.set_password(password)
+        account.save()
+        return account
--- backend/account/api/urls.py
+++ backend/account/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from account.api.views import LoginView, AccountsView
+from account.api.views import LoginView, AccountsView, CreateAccountView
 
 urlpatterns = [
     path('', AccountsView.as_view()),
     path('login', LoginView.as_view()),
+    path('register', CreateAccountView.as_view())
 ]
