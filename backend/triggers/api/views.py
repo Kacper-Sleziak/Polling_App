ae402cdf80377c0377bbdb7310e4737abcfee27b
Kacper <kacper-sleziak@wp.pl>
2022-03-22 14:16:53 +0100

    Code clenaup with pep8 extension

--- backend/triggers/api/views.py
+++ backend/triggers/api/views.py
@@ -6,89 +6,91 @@ from triggers.models import Trigger
 from questions.models import Question
 from triggers.api.serializers import TriggerSerializer
 
-# [POST] Creating Trigger 
+# [POST] Creating Trigger
+
+
 class CreateTriggerView(APIView):
     serializer_class = TriggerSerializer
-    
+
     def post(self, request, format=None):
         serializer = self.serializer_class(data=request.data)
-        
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
 # [GET, DELETE] Trigger View
+
+
 class TriggerView(APIView):
     serializer_class = TriggerSerializer
-    
+
     def get_trigger(self, pk):
         queryset = Trigger.objects.filter(id=pk)
-        
+
         if queryset.exists():
             trigger = queryset[0]
             return trigger
         else:
             return 0
-    
+
     def get(self, request, pk, format=None):
         serializer = self.serializer_class
         trigger = self.get_trigger(pk)
-        
-        if trigger !=0:
-            return Response(serializer(trigger).data, status=status.HTTP_200_OK)
+
+        if trigger != 0:
+            return Response(
+                serializer(trigger).data,
+                status=status.HTTP_200_OK)
         return Response(status=status.HTTP_204_NO_CONTENT)
-            
-    
+
     def delete(self, request, pk, format=None):
         trigger = self.get_trigger(pk)
-        
-        if trigger !=0:
+
+        if trigger != 0:
             trigger.delete()
             trigger.save()
             return Response(status=status.HTTP_200_OK)
         return Response(status=status.HTTP_204_NO_CONTENT)
-    
+
     def put(self, request, pk, format=None):
         serializer = self.serializer_class(data=request.data)
         trigger = self.get_trigger(pk)
-        
-        if trigger !=0:
+
+        if trigger != 0:
             if serializer.is_valid():
                 serializer.update(trigger, serializer.validated_data)
-                return Response(self.serializer_class(trigger).data, status.HTTP_200_OK)
+                return Response(
+                    self.serializer_class(trigger).data,
+                    status.HTTP_200_OK)
             return Response(staus=status.HTTP_400_BAD_REQUEST)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 # [GET] Get all triggers for given question view
+
+
 class GetTriggersForQuestion(APIView):
     serializer_class = TriggerSerializer
-    
+
     def get_question(self, pk):
         queryset = Question.objects.filter(id=pk)
-        
+
         if queryset.exists():
             question = queryset[0]
             return question
         else:
             return 0
-        
+
     def get(self, request, pk, format=None):
         serializer = self.serializer_class
-        
-        if self.get_question(pk) !=0:
+
+        if self.get_question(pk) != 0:
             triggers = Trigger.objects.filter(triggered_question=pk)
             if triggers.exists():
                 response_data = serializer(triggers, many=True).data
                 return Response(response_data, status=status.HTTP_200_OK)
-            return Response({'triggers': 'no triggers for this question'}, status=status.HTTP_204_NO_CONTENT)
-        return Response({'email': 'there is no email with given id'}, status=status.HTTP_204_NO_CONTENT)
-        
-
-        
-
-
-
-        
-        
-
+            return Response(
+                {'triggers': 'no triggers for this question'}, status=status.HTTP_204_NO_CONTENT)
+        return Response({'email': 'there is no email with given id'},
+                        status=status.HTTP_204_NO_CONTENT)
--- backend/triggers/models.py
+++ backend/triggers/models.py
@@ -2,21 +2,31 @@ from django.db import models
 from questions.models import Question
 from django.core.exceptions import ValidationError
 
+
 class Trigger(models.Model):
-    triggering_answer = models.CharField(verbose_name="triggering answer", max_length=200)
-    triggering_question = models.ForeignKey(Question, related_name ='%(class)s_triggering', on_delete=models.CASCADE)
-    triggered_question = models.ForeignKey(Question, related_name ='%(class)s_triggered', on_delete=models.CASCADE)
-    
+    triggering_answer = models.CharField(
+        verbose_name="triggering answer", max_length=200)
+    triggering_question = models.ForeignKey(
+        Question,
+        related_name='%(class)s_triggering',
+        on_delete=models.CASCADE)
+    triggered_question = models.ForeignKey(
+        Question,
+        related_name='%(class)s_triggered',
+        on_delete=models.CASCADE)
+
     def clean(self):
-        
-        # Checking if 2 questions belong to same poll 
+
+        # Checking if 2 questions belong to same poll
         if self.triggering_question.poll != self.triggered_question.poll:
             raise ValidationError(
                 {'triggering_question': 'Triggers must be related with same poll!'})
-        
-        # Checking if 2 questions are triggering each other 
-        # For example question1 is triggeringg question2 and question2 is triggering question2
-        queryset = Trigger.objects.filter(triggering_question=self.triggered_question)
+
+        # Checking if 2 questions are triggering each other
+        # For example question1 is triggeringg question2 and question2 is
+        # triggering question2
+        queryset = Trigger.objects.filter(
+            triggering_question=self.triggered_question)
         if queryset.exists():
             raise ValidationError(
                 {'triggering_question': 'Two questions can not trigger each other!'})
