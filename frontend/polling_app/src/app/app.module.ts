2803ce55e264f943344a82dc087e3b9813ce3271
wiktord2000 <wiktord2000@wp.pl>
2022-03-25 14:21:44 +0100

    Add material modul

--- frontend/polling_app/src/app/app.module.ts
+++ frontend/polling_app/src/app/app.module.ts
@@ -1,33 +1,32 @@
+// Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { PollingFormComponent } from './components/form-components/polling-form/polling-form.component';
-import { FormTitleComponent } from './components/form-components/form-title/form-title.component';
-import { QuestionComponent } from './components/form-components/question/question.component';
-import { AnswerComponent } from './components/form-components/answers/answer/answer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCardModule} from '@angular/material/card'
-import {MatCheckboxModule} from '@angular/material/checkbox'
-import {MatButtonModule} from '@angular/material/button';
-import {MatSelectModule} from '@angular/material/select';
-import {MatRadioModule} from '@angular/material/radio'
-import {MatSlideToggleModule} from '@angular/material/slide-toggle'
-import {MatFormFieldModule} from '@angular/material/form-field'
-import {MatInputModule} from '@angular/material/input'
-import { CheckboxAnswerComponent } from './components/form-components/answers/checkbox-answer/checkbox-answer.component';
-import { ComboboxAnswerComponent } from './components/form-components/answers/combobox-answer/combobox-answer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from './material/material.module';
+
+// App component
+import { AppComponent } from './app.component';
+//
+// Form components
 import { FormAnswersComponent } from './components/form-components/form-answers/form-answers.component';
 import { TextAnswerComponent } from './components/form-components/answers/text-answer/text-answer.component';
 import { TrueOrFalseAnswerComponent } from './components/form-components/answers/true-or-false-answer/true-or-false-answer.component';
 import { RadioAnswerComponent } from './components/form-components/answers/radio-answer/radio-answer.component';
 import { ShortTextAnswerComponent } from './components/form-components/answers/short-text-answer/short-text-answer.component';
+import { PollingFormComponent } from './components/form-components/polling-form/polling-form.component';
+import { FormTitleComponent } from './components/form-components/form-title/form-title.component';
+import { QuestionComponent } from './components/form-components/question/question.component';
+import { AnswerComponent } from './components/form-components/answers/answer/answer.component';
+import { CheckboxAnswerComponent } from './components/form-components/answers/checkbox-answer/checkbox-answer.component';
+import { ComboboxAnswerComponent } from './components/form-components/answers/combobox-answer/combobox-answer.component';
+//
+//Shared components
 import { FooterComponent } from './components/shared-components/footer/footer.component';
 import { ToolbarComponent } from './components/shared-components/toolbar/toolbar.component';
 import { SideNavigatorComponent } from './components/shared-components/side-navigator/side-navigator.component'
-
+//
 
 
 @NgModule({
@@ -52,14 +51,8 @@ import { SideNavigatorComponent } from './components/shared-components/side-navi
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCardModule,
-    MatCheckboxModule,
-    MatButtonModule,
-    MatSelectModule,
-    MatRadioModule,
-    MatSlideToggleModule,
-    MatFormFieldModule,
-    MatInputModule
+    HttpClientModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/polling_app/src/app/material/material.module.ts
@@ -0,0 +1,56 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider'; 
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input'; 
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTooltipModule } from '@angular/material/tooltip'; 
+import { MatCardModule } from '@angular/material/card'
+import { MatCheckboxModule } from '@angular/material/checkbox'
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio'
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
+import { MatFormFieldModule } from '@angular/material/form-field'
+
+
+const MaterialComponents = [
+
+  MatToolbarModule,
+  MatSidenavModule,
+  MatDividerModule,
+  MatIconModule,
+  MatListModule,
+  MatTabsModule,
+  MatButtonModule,
+  MatTableModule,
+  MatInputModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatTooltipModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatSelectModule,
+  MatRadioModule,
+  MatSlideToggleModule,
+  MatFormFieldModule
+  
+];
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MaterialComponents
+  ],
+  exports: [
+    MaterialComponents
+  ]
+})
+
+export class MaterialModule { }
