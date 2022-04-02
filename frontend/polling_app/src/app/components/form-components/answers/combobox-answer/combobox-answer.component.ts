2bba04d1f30c5e903182cf95f26a0431fdccb87d
taydexx <jakub.tadeusz@interia.pl>
2022-04-02 14:55:46 +0200

    Implement change event in combobox component

--- frontend/polling_app/src/app/components/form-components/answers/combobox-answer/combobox-answer.component.ts
+++ frontend/polling_app/src/app/components/form-components/answers/combobox-answer/combobox-answer.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit } from '@angular/core';
+import { MatOptionSelectionChange } from '@angular/material/core';
 import { Answer } from 'src/app/models/form-models/answer';
+import { Result } from 'src/app/models/form-models/result';
 import { AnswerComponent } from '../answer/answer.component';
 
 @Component({
@@ -9,4 +11,14 @@ import { AnswerComponent } from '../answer/answer.component';
 })
 export class ComboboxAnswerComponent extends AnswerComponent {
 
+  override ngOnInit(): void {
+    const options: []= [];
+    this.result = new Result(this.answerId, options);
+  }
+
+  updateResult = (event: MatOptionSelectionChange) => {
+    const result = this.result;
+    result.options = [{optionId: event.source.value.id, content:event.source.value.isChecked.toString()}];
+    this.resultChanged.emit(result);
+  }
 }
