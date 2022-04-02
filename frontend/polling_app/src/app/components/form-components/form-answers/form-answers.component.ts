a4369bf1b2a2f27b1a9084ae95ef51f5921f02e3
taydexx <jakub.tadeusz@interia.pl>
2022-04-02 14:57:00 +0200

    Implement change event handling in form answers component

--- frontend/polling_app/src/app/components/form-components/form-answers/form-answers.component.ts
+++ frontend/polling_app/src/app/components/form-components/form-answers/form-answers.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Answer} from 'src/app/models/form-models/answer';
 import { QuestionType } from 'src/app/models/form-models/question';
+import { Result } from 'src/app/models/form-models/result';
 
 @Component({
   selector: 'app-form-answers',
@@ -10,7 +11,13 @@ import { QuestionType } from 'src/app/models/form-models/question';
 export class FormAnswersComponent implements OnInit {
   @Input() answers: Answer[] = [];
   @Input() type: QuestionType = QuestionType.Checkbox;
+  @Input() questionId!: number;
   questionType: typeof QuestionType = QuestionType;
+
+
+  onResultChanged = (event: Result) => {
+    console.log(event);
+  }
 
   constructor() { }
 
