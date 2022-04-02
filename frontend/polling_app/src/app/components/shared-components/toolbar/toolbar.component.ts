783070a39f4c685c6a87f2224df4af44e9d72805
wiktord2000 <wiktord2000@wp.pl>
2022-04-02 15:01:57 +0200

    Modify toolbar

--- frontend/polling_app/src/app/components/shared-components/toolbar/toolbar.component.ts
+++ frontend/polling_app/src/app/components/shared-components/toolbar/toolbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+
+  hasRoute(route: string): boolean{
+    return this.router.url.includes(route);
   }
 
 }
