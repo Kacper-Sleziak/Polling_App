631130362d2a11ab25a1c1836037050a96e6b1cf
taydexx <jakub.tadeusz@interia.pl>
2022-04-02 14:56:37 +0200

    Implement change event in true/false component

--- frontend/polling_app/src/app/components/form-components/answers/true-or-false-answer/true-or-false-answer.component.ts
+++ frontend/polling_app/src/app/components/form-components/answers/true-or-false-answer/true-or-false-answer.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 import { AnswerComponent } from '../answer/answer.component';
+import {Answer} from '../../../../models/form-models/answer'
+import { Result } from 'src/app/models/form-models/result';
 
 @Component({
   selector: 'app-true-or-false-answer',
@@ -8,5 +11,13 @@ import { AnswerComponent } from '../answer/answer.component';
 })
 export class TrueOrFalseAnswerComponent extends AnswerComponent {
 
+  updateResult = (event: MatSlideToggleChange, answer: Answer) => {
+    answer.isChecked = true;
+    const options: {optionId: number, content: string}[] = [];
+    for (const answer of this.answers) {
+      options.push({optionId: answer.id, content: answer.isChecked.toString()});
+    }
+    this.resultChanged.emit(new Result(this.answerId, options));
+  }
 
 }
