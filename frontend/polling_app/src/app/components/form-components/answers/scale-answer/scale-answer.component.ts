f84fe7893be30134f35e2bcdd8b7a85c226ffe09
taydexx <jakub.tadeusz@interia.pl>
2022-04-02 14:56:06 +0200

    Implement change event in scale answer component

--- frontend/polling_app/src/app/components/form-components/answers/scale-answer/scale-answer.component.ts
+++ frontend/polling_app/src/app/components/form-components/answers/scale-answer/scale-answer.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatRadioChange } from '@angular/material/radio';
+import { Result } from 'src/app/models/form-models/result';
 import { AnswerComponent } from '../answer/answer.component';
 
 @Component({
@@ -10,10 +12,25 @@ export class ScaleAnswerComponent extends AnswerComponent {
   @Input() scale: number = 5;
   results: boolean [] = [];
 
+  updateResult = (event: MatRadioChange) =>{
+    for (const option of this.result.options) {
+      if(option.optionId === event.value.optionId){
+        option.content = event.value.content.toString();
+      }
+    }
+    this.resultChanged.emit(this.result);
+  }
+
   override ngOnInit(): void {
     this.results = [];
     for(let i = 0; i < this.scale; i++){
       this.results.push(false);
     }
+    const options: {optionId: number, content: string}[] = [];
+    for (const answer of this.answers) {
+      options.push({optionId: answer.id, content: ""});  
+    }
+
+    this.result = new Result(this.answerId, options);
   }
 }
