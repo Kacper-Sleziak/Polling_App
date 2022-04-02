3748f9092f3e5ccd55e2c7ddaa4b6bae68ddcc9d
taydexx <jakub.tadeusz@interia.pl>
2022-04-02 14:56:27 +0200

    Implement change event in text/shorttext answer components

--- frontend/polling_app/src/app/components/form-components/answers/text-answer/text-answer.component.ts
+++ frontend/polling_app/src/app/components/form-components/answers/text-answer/text-answer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatInput } from '@angular/material/input';
+import { Answer } from 'src/app/models/form-models/answer';
+import { Result } from 'src/app/models/form-models/result';
 import { AnswerComponent } from '../answer/answer.component';
 
 @Component({
@@ -8,5 +11,15 @@ import { AnswerComponent } from '../answer/answer.component';
 })
 export class TextAnswerComponent extends AnswerComponent {
 
+  updateResult = (event: Event, answer: Answer) => {
+    const target = event.target as HTMLInputElement;
+    answer.result = target.value
 
+    const options: {optionId: number, content: string}[] = [];
+    for (const answer of this.answers) {
+      options.push({optionId: answer.id, content: answer.result});  
+    }
+    this.result = new Result(this.answerId, options);
+    this.resultChanged.emit(this.result);
+  }
 }
