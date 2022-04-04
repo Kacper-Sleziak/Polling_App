5b108f9190aa937f3caec6b022124d814d1f886d
wiktord2000 <wiktord2000@wp.pl>
2022-04-04 20:51:58 +0200

    Edit poll miniature

--- frontend/polling_app/src/app/pages/polls/components/card/card.component.ts
+++ frontend/polling_app/src/app/pages/polls/components/card/card.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog} from '@angular/material/dialog';
+import { Poll } from 'src/app/models/dashboard-models/poll';
+import { PollService } from 'src/app/services/dashboard-services/poll.service';
+import { DeleteDialogComponent } from '../delete-dialog/delete-dialog.component';
 
 @Component({
   selector: 'app-card',
@@ -8,11 +12,24 @@ import { Component, Input, OnInit } from '@angular/core';
 export class CardComponent implements OnInit {
 
 
-  @Input() text!: string;
-  @Input() status!: string;
-  @Input() id!: string;
+  // @Input() text!: string;
+  // @Input() status!: string;
+  // @Input() id!: number;
 
-  constructor() { }
+  @Input() poll !: Poll;
+
+  constructor(public dialog: MatDialog, private pollService: PollService) { }
+
+  openDeleteDialog(): void{
+    const dialogRef = this.dialog.open(DeleteDialogComponent, {data: {pollId : this.poll.id}});
+
+    dialogRef.afterClosed().subscribe((result : boolean) => {
+      if(result === true){
+        this.pollService.deletePoll(this.poll.id);
+      }
+      // console.log(`Dialog result: ${result}`);
+    });
+  }
 
   ngOnInit(): void {
   }
