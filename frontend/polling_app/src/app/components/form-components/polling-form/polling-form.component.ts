e172064104c5dc36364b33bfdc81386608d91008
taydexx <jakub.tadeusz@interia.pl>
2022-04-08 11:19:23 +0200

    add example fetch

--- frontend/polling_app/src/app/components/form-components/polling-form/polling-form.component.ts
+++ frontend/polling_app/src/app/components/form-components/polling-form/polling-form.component.ts
@@ -39,6 +39,9 @@ export class PollingFormComponent implements OnInit {
 
   handleSendAnswer = () => {
     console.log(this.getResultInJSON());
+    /*fetch('http://127.0.0.1:8000/polls/1/')
+    .then(response => response.json())
+    .then(data => console.log(data));*/
   }
 
 }
