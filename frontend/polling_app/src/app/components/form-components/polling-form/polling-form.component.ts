1c728cebb08bb7cc5dd3df6c0d33ca9b37fb5e85
taydexx <jakub.tadeusz@interia.pl>
2022-04-08 13:50:36 +0200

    Add services usage to the polling form component

--- frontend/polling_app/src/app/components/form-components/polling-form/polling-form.component.ts
+++ frontend/polling_app/src/app/components/form-components/polling-form/polling-form.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Output } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Question } from 'src/app/models/form-models/question';
 import { Result } from 'src/app/models/form-models/result';
+import { PollService } from 'src/app/services/dashboard-services/poll.service';
+import { AnswerService } from 'src/app/services/form-services/answer.service';
 import { QuestionsService } from 'src/app/services/form-services/questions.service';
 
 @Component({
@@ -10,14 +13,29 @@ import { QuestionsService } from 'src/app/services/form-services/questions.servi
 })
 export class PollingFormComponent implements OnInit {
   questions: Question[] = [];
-  @Output() title: string = 'Przykładowy tytuł';
-  @Output() description: string = 'Przykładowy opis';
+  title: string = 'Przykładowy tytuł';
+  description: string = 'Przykładowy opis';
   questionResult: Map<Question, Result> = new Map();
 
-  constructor(private questionsService: QuestionsService) {}
+  constructor(private questionsService: QuestionsService, private pollService: PollService, private answerService: AnswerService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.questionsService.getQuestions().subscribe(questions=>this.questions = questions);
+    const slug = this.route.snapshot.paramMap.get('slug');
+    const pollId = 1;
+    if(slug !== null){
+      this.pollService.getPoll(slug).subscribe(poll=>{
+        this.title = poll.name;
+        this.description = poll.description;
+        this.questionsService.getQuestions(poll.id).subscribe(
+          questions=>{
+            this.questions = questions;
+            for (const question of this.questions) {
+              this.answerService.getAnswers(question.id).subscribe(answers=>question.answers = answers);
+            }
+          }
+        );
+      });
+    }
   }
 
   updateAnswer = (question: Question, result: Result) => {
@@ -38,7 +56,8 @@ export class PollingFormComponent implements OnInit {
   }
 
   handleSendAnswer = () => {
-    console.log(this.getResultInJSON());
+    //console.log(this.getResultInJSON());
+    console.log(this.questionResult);
     /*fetch('http://127.0.0.1:8000/polls/1/')
     .then(response => response.json())
     .then(data => console.log(data));*/
