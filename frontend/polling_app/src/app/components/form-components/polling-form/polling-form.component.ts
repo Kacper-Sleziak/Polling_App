d84d876d0d38270458075146b3a6cc3f71d7567d
taydexx <jakub.tadeusz@interia.pl>
2022-04-10 13:51:41 +0200

    Move logic between questions from polling-form component to questionViewModelService

--- frontend/polling_app/src/app/components/form-components/polling-form/polling-form.component.ts
+++ frontend/polling_app/src/app/components/form-components/polling-form/polling-form.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit, Output } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Question } from 'src/app/models/form-models/question';
 import { Result } from 'src/app/models/form-models/result';
 import { PollService } from 'src/app/services/dashboard-services/poll.service';
 import { AnswerService } from 'src/app/services/form-services/answer.service';
+import { QuestionViewModelService } from 'src/app/services/form-services/question-view-model.service';
 import { QuestionsService } from 'src/app/services/form-services/questions.service';
 
 @Component({
   selector: 'app-polling-form',
   templateUrl: './polling-form.component.html',
-  styleUrls: ['./polling-form.component.css']
+  styleUrls: ['./polling-form.component.css'],
 })
 export class PollingFormComponent implements OnInit {
   questions: Question[] = [];
@@ -17,50 +19,36 @@ export class PollingFormComponent implements OnInit {
   description: string = 'Przykładowy opis';
   questionResult: Map<Question, Result> = new Map();
 
-  constructor(private questionsService: QuestionsService, private pollService: PollService, private answerService: AnswerService, private route: ActivatedRoute) {}
+  constructor(
+    private pollService: PollService,
+    private questionViewModelService: QuestionViewModelService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     const slug = this.route.snapshot.paramMap.get('slug');
-    const pollId = 1;
-    if(slug !== null){
-      this.pollService.getPoll(slug).subscribe(poll=>{
+    if (slug !== null) {
+      this.pollService.getPoll(slug).subscribe((poll) => {
         this.title = poll.name;
         this.description = poll.description;
-        this.questionsService.getQuestions(poll.id).subscribe(
-          questions=>{
-            this.questions = questions;
-            for (const question of this.questions) {
-              this.answerService.getAnswers(question.id).subscribe(answers=>question.answers = answers);
-            }
-          }
-        );
+        this.questionViewModelService.loadPollQuestions(poll.id);
+        this.questionViewModelService
+          .onUpdate()
+          .subscribe((q: Question[]) => (this.questions = q));
       });
     }
   }
 
   updateAnswer = (question: Question, result: Result) => {
-    this.questionResult.set(question, result);
-  }
-
-  getResultInJSON = () => {
-    let arr = [];
-    for (const key of this.questionResult.keys()) {
-      let res = {
-      questionId: key.id,
-      question: key.question,
-      type: key.type,
-      results: this.questionResult.get(key)};
-      arr.push(res);
-    }
-    return JSON.stringify(arr);
-  }
+    this.questionViewModelService.updateAnswer(question, result);
+    this.questions = this.questionViewModelService.getQuestions();
+  };
 
   handleSendAnswer = () => {
     //console.log(this.getResultInJSON());
-    console.log(this.questionResult);
+    console.log(this.questionViewModelService.getResultInJSON());
     /*fetch('http://127.0.0.1:8000/polls/1/')
     .then(response => response.json())
     .then(data => console.log(data));*/
-  }
-
+  };
 }
