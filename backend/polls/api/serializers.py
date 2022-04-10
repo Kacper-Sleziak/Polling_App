0a0136980fca57d123adcddecb9f40dcb45ca11c
Pasti29 <karolpastewski29@gmail.com>
2022-04-10 23:42:20 +0200

    Added validation for dates

--- backend/polls/api/serializers.py
+++ backend/polls/api/serializers.py
@@ -1,8 +1,17 @@
 from polls.models import Poll
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, ValidationError
 
 
 class PollSerializer(ModelSerializer):
+
+    def validate(self, data):
+        if data['start_date'] == None and data['end_date'] != None:
+            raise ValidationError("Can't specify end date without start date")
+
+        if data['start_date'] > data['end_date']:
+            raise ValidationError("End date can't be before start date")
+        return data
+
     class Meta:
         model = Poll
         fields = ('__all__')
