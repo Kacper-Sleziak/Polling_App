e4fa8cf268c110bdc4bca697361ced795339f7d8
Kacper <kacper-sleziak@wp.pl>
2022-04-14 17:56:16 +0200

    commit

---
+++ backend/pdf_results_exporter/api/views.py
@@ -0,0 +1,36 @@
+from pdf_results_exporter.api.renderers import BinaryFileRenderer
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.renderers import JSONRenderer
+from Polling_App.settings import BASE_DIR
+from polls.models import Poll
+from pdf_results_exporter.exporter_pdf import generate_pdf
+import os
+
+
+
+'''
+This view return pdf with results of poll
+'''
+
+class GetPdfView(APIView):
+    renderer_classes = [BinaryFileRenderer]
+    
+    def get(self, request, pk, format=None):
+        
+        queryset = Poll.objects.filter(id=pk)
+        
+        if queryset.exists():
+            poll = queryset[0]
+            pdf_path = generate_pdf(poll)
+            
+            with open(pdf_path, 'rb') as report:
+                return Response(
+                    report.read(),
+                    headers={'Content-Disposition': 
+                        'attachment; filename="file.pdf"'},
+                    content_type='application/pdf',
+                    status=status.HTTP_200_OK
+                    )
+        return Response(status=status.HTTP_404_NOT_FOUND)
