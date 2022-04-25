ef577924d90d07e57521398caf6c8a684248af67
taydexx <jakub.tadeusz@interia.pl>
2022-04-25 23:01:02 +0200

    Minor fix

--- frontend/polling_app/src/app/components/form-components/question-edit-controls/question-edit-controls.component.ts
+++ frontend/polling_app/src/app/components/form-components/question-edit-controls/question-edit-controls.component.ts
@@ -12,7 +12,7 @@ export class QuestionEditControlsComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  handleCopyButton = (event: Event) => {
+  handleCopyButton = () => {
     this.onQuestionCopy.emit();
   };
 
