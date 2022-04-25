208f1de92e2af64e68e037bb4d7e04c62da8e8d0
taydexx <jakub.tadeusz@interia.pl>
2022-04-25 14:29:25 +0200

    Fix error with account logo migration

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,4 +1,3 @@
-from asyncio.windows_events import NULL
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
@@ -48,7 +47,7 @@ class Account(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-    logo = models.ImageField(upload_to=logo_upload_location, default=NULL, null=True, blank=True)
+    logo = models.ImageField(upload_to=logo_upload_location, default=None, null=True, blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['company_name']
