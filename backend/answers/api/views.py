7ff5b91d052ac6e15dd29a64f9ae54d1b6118381
Kacper <kacper-sleziak@wp.pl>
2022-05-08 17:27:15 +0200

    Created api tests for account

--- backend/answers/api/views.py
+++ backend/answers/api/views.py
@@ -100,7 +100,7 @@ class CreateAnswerView(generics.CreateAPIView):
 
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
