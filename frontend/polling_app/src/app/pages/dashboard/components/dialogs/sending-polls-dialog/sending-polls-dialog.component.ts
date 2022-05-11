1011097a78cb9a931c2cbbc9a9b23ed60dee68b2
wiktord2000 <wiktord2000@wp.pl>
2022-05-11 19:37:52 +0200

    Add "Delete all" and "Undo" button in poll sending

--- frontend/polling_app/src/app/pages/dashboard/components/dialogs/sending-polls-dialog/sending-polls-dialog.component.ts
+++ frontend/polling_app/src/app/pages/dashboard/components/dialogs/sending-polls-dialog/sending-polls-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { NgxCsvParser, NgxCSVParserError } from 'ngx-csv-parser';
 import { Email } from 'src/app/models/dashboard-models/email';
 import { MailService } from 'src/app/services/dashboard-services/mail.service';
 import { CustomSnackBarComponent } from './components/custom-snack-bar/custom-snack-bar.component';
@@ -16,11 +17,16 @@ export class SendingPollsDialogComponent implements OnInit {
   constructor(  @Inject(MAT_DIALOG_DATA) public data: {pollSlug: string}, 
                 private formBuilder: FormBuilder, 
                 private mailService: MailService,
-                private snackBar: MatSnackBar) {}
+                private snackBar: MatSnackBar,
+                private ngxCsvParser: NgxCsvParser) {}
 
-  emails : string[] = []
+  emails : string[] = [];
   isSubject: boolean = false;
   isRecipient: boolean = false;
+  selectedFile: any = null;
+  removeHistory: string[] = []
+  allEmailsRemoveHistory: Array<Array<string>> = [];
+
 
   emailForm : FormGroup = this.formBuilder.group(
     {
@@ -56,7 +62,6 @@ export class SendingPollsDialogComponent implements OnInit {
         return false;
       })
 
-      console.log(filteredArray);
       if(filteredArray.length == 0){
         this.emails.push(email);
       }
@@ -71,7 +76,11 @@ export class SendingPollsDialogComponent implements OnInit {
   onDeleteEmail(emailToRemove : string){
     // Delete email from array
     this.emails = this.emails.filter((email)=>{
-      if(email === emailToRemove) return false;
+      if(email === emailToRemove) 
+      {
+        this.removeHistory.push(emailToRemove);
+        return false;
+      }
       return true;
     })
 
@@ -111,4 +120,51 @@ export class SendingPollsDialogComponent implements OnInit {
     });
   }
 
+
+  onFileSelected(event: any): void {
+
+    this.selectedFile = event.target.files[0] ?? null;
+    
+  }
+
+
+  onRemoveAllEmails(): void{
+
+    if(this.emails.length){
+      // Ad "ALL" tag to signal that all emails have been deleted
+      this.removeHistory.push("ALL");
+      this.allEmailsRemoveHistory.push(this.emails);
+      this.emails = [];
+      this.isRecipient = false;
+    }
+  }
+
+
+  onReturnEmails(): void{
+
+    // If history isn't empty
+    if(this.removeHistory.length){
+
+      // Pop from array. We will get: email or string "ALL"
+      let lastDeletedEmail = this.removeHistory.pop()!;
+
+      // We have to add group of emails
+      if(lastDeletedEmail === 'ALL'){
+
+        if(this.allEmailsRemoveHistory.length){
+          let lastGroupRemove = this.allEmailsRemoveHistory.pop()!;
+          this.emails = [...new Set([...this.emails , ...lastGroupRemove])];
+        }
+
+      }
+      // Otherwise we have to add only one email
+      else{
+        this.emails = [...new Set([...this.emails , lastDeletedEmail])];
+      }
+
+      this.isRecipient = true;
+    }
+  }
 }
+
+
