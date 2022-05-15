b93498cf12667e37b7cd74d9643aa6696929dbc3
wiktord2000 <wiktord2000@wp.pl>
2022-05-15 20:07:44 +0200

    Update login panel

--- frontend/polling_app/src/app/pages/login-panel/login-panel.component.ts
+++ frontend/polling_app/src/app/pages/login-panel/login-panel.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AccountService } from 'src/app/services/shared-services/account.service';
 
 @Component({
   selector: 'app-login-panel',
@@ -8,7 +10,15 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginPanelComponent implements OnInit {
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, 
+              private accountService: AccountService,
+              private router: Router) {
+    
+    // E.g. when use backword in dashboard and redirect to login panel - we want logout account
+    this.accountService.logoutAccount();
+  }
+
+  showPassword: boolean = false;
 
   loginForm : FormGroup = this.formBuilder.group(
     {
@@ -18,12 +28,37 @@ export class LoginPanelComponent implements OnInit {
   );
 
   ngOnInit(): void {
-    this.loginForm.controls['email'].addValidators([Validators.email]);
+    // Set input validators
+    this.loginForm.controls['email'].addValidators([Validators.email, Validators.required]);
+    this.loginForm.controls['password'].addValidators([Validators.required]);
   }
 
   onLoginButtonClick(){
-    console.log("elo");
-    
+    // If form is valid
+    if(this.loginForm.valid){
+      // Send request
+      this.accountService.postLogin(this.loginForm.controls['email'].value, this.loginForm.controls['password'].value)
+      .subscribe({
+          // If success
+          next: (account) => {
+            this.accountService.setAuthorId(account.id);
+            // Navigate to dashboard
+            this.router.navigate(['dashboard']);
+          },
+          // If error
+          error: () =>{
+            if(document.getElementById('error-message') !== null){
+              document.getElementById('error-message')!.innerHTML = "Hasło lub email jest niepoprawne!";
+            }
+          }
+
+        }
+      )
+    }
+  }
+
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
   }
 
 }
