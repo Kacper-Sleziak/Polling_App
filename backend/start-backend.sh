f143f7cc7fdd5cf45dca20f76a0ebdfc1707ab25
Pasti29 <karolpastewski29@gmail.com>
2022-05-19 19:37:44 +0200

    Potential fix to wait_for_db.py script?

--- backend/start-backend.sh
+++ backend/start-backend.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+cp /django/custom/commands/wait_for_db.py /usr/local/lib/python3.10/site-packages/django/core/management/commands/
+
 python3 /django/manage.py wait_for_db
 python3 /django/manage.py migrate
 python3 /django/manage.py loaddata /django/share/data.json
