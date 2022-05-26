50d17169a11aea3b6a134aab8ef0a9d852733ff6
Pasti29 <karolpastewski29@gmail.com>
2022-05-26 20:41:46 +0200

    Moved gunicorn config to seperate file

--- backend/start-backend.sh
+++ backend/start-backend.sh
@@ -4,4 +4,4 @@
 python3 /django/manage.py wait_for_db
 python3 /django/manage.py migrate
 python3 /django/manage.py loaddata /django/share/data.json
-gunicorn Polling_App.wsgi --bind 0.0.0.0:8000
+gunicorn Polling_App.wsgi
