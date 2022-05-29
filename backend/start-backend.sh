a60ac01b71edca5977842192b2c8b2b37ecaa838
Pasti29 <karolpastewski29@gmail.com>
2022-05-29 14:11:11 +0200

    Added auto makemigrations to start-backend script

--- backend/start-backend.sh
+++ backend/start-backend.sh
@@ -2,6 +2,7 @@
 
 
 python3 /django/manage.py wait_for_db
+python3 /django/manage.py makemigrations
 python3 /django/manage.py migrate
 python3 /django/manage.py loaddata /django/share/data.json
 python3 /django/manage.py runserver 0.0.0.0:8000
