506648826da4045d13cd6f265bec6fecf5c05ee0
Pasti29 <karolpastewski29@gmail.com>
2022-05-30 17:34:04 +0200

    Added a new endpoint for duplicating polls

--- backend/polls/api/urls.py
+++ backend/polls/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from polls.api.views import PollForAuthorList, PollDetail, CreatePoll
+from polls.api.views import PollForAuthorList, PollDetail, CreatePoll, DuplicatePoll
 
 urlpatterns = [
     path("", CreatePoll.as_view()),
     path("<slug:slug>/", PollDetail.as_view()),
     path("author/<int:author_id>/", PollForAuthorList.as_view()),
+    path("duplicate/<slug:slug>/", DuplicatePoll.as_view()),
 ]
