27e9ea48d89c0ba8caadbde2aa4da8c6cd7aca33
wiktord2000 <wiktord2000@wp.pl>
2022-05-30 18:01:54 +0200

    Prepare new models to map json data

--- frontend/polling_app/src/app/models/results/question-stats.ts
+++ frontend/polling_app/src/app/models/results/question-stats.ts
@@ -1,23 +1,20 @@
-import { Answer } from "../form-models/answer";
 import { QuestionType } from "../form-models/question";
 import { AnswerStats } from "./answer-stats";
 
 
 export class QuestionStats {
 
-    question: string;
+    questionName: string;
+    questionType: QuestionType;
+    required: boolean;
+    answerCount: number;
     answersStats: AnswerStats[];
 
-    // id: number = -1;
-    // position: number = 0;
-    // question!: string;
-    // answers: Answer[] = [];
-    // type: QuestionType;
-
-
-
-    constructor(question: string, answersStats: AnswerStats[]){
-        this.question = question;
+    constructor(questionName: string,  questionType: QuestionType, required: boolean, answerCount: number, answersStats: AnswerStats[]){
+        this.questionName = questionName;
+        this.questionType = questionType;
+        this.required = required;
+        this.answerCount = answerCount;
         this.answersStats = answersStats;
     }
 }
