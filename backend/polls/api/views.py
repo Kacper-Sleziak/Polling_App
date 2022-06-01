78f0f2a178349a3a88a8b092da85dd5b33201404
Pasti29 <karolpastewski29@gmail.com>
2022-06-01 17:34:20 +0200

    Duplicate endpoint now return a object that was added

--- backend/polls/api/views.py
+++ backend/polls/api/views.py
@@ -104,6 +104,4 @@ class DuplicatePoll(generics.GenericAPIView):
         copied_poll.save()
         serializer = self.serializer_class(copied_poll)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-        # copied_poll.save()
-        # return HttpResponse(status=status.HTTP_201_CREATED)
         
