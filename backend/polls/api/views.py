87623d637e7456863dce1ab78a4540224c5ec035
Pasti29 <karolpastewski29@gmail.com>
2022-06-04 00:22:10 +0200

    Correct coping options and questions with duplicating a poll

--- backend/polls/api/views.py
+++ backend/polls/api/views.py
@@ -2,6 +2,7 @@ from django.core import serializers
 from django.http import Http404, HttpResponse
 from polls.api.serializers import PollSerializer
 from polls.models import Poll
+from questions.models import Question, Option
 from rest_framework import status, generics
 from rest_framework.response import Response
 
@@ -79,7 +80,7 @@ class CreatePoll(generics.GenericAPIView):
 class DuplicatePoll(generics.GenericAPIView):
     serializer_class = PollSerializer
     
-    def get_object(self, slug):
+    def get_poll(self, slug):
         """
         Helper method that return a poll by it's slug
         """
@@ -87,21 +88,57 @@ class DuplicatePoll(generics.GenericAPIView):
             return Poll.objects.get(slug=slug)
         except Poll.DoesNotExist:
             raise Http404
+        
+    def get_questions(self, poll_id):
+        try:
+            return Question.objects.filter(poll=poll_id)
+        except:
+            raise Http404
+    
+    def get_options(self, question_id):
+        try:
+            return Option.objects.filter(question=question_id)
+        except:
+            raise Http404
     
     def get(self, request, slug, format=None):
         """
         Duplicate poll
         """
-        copied_poll = self.get_object(slug)
-        copied_poll.id = None
-        copied_poll.slug = None
-        copied_poll.create_date = None
-        copied_poll.start_date = None
-        copied_poll.end_date = None
-        copied_poll.filling = 0
-        copied_poll.sent = 0
-        copied_poll.status = 2
-        copied_poll.save()
-        serializer = self.serializer_class(copied_poll)
+        copied_poll = self.get_poll(slug)
+        poll = Poll(
+            title = copied_poll.title,
+            description = copied_poll.description,
+            status = 2,
+            author = copied_poll.author
+        )
+        serializer = self.serializer_class(poll)
+        poll.save()
+        
+        
+        question_set = self.get_questions(copied_poll.id)
+        for question in question_set:
+            new_question = Question(
+                position = question.position,
+                content = question.content,
+                poll = poll,
+                question_type = question.question_type,
+                required = question.required
+            )
+            try:
+                new_question.save()
+            except ValueError:
+                raise Http404
+            option_set = self.get_options(question.id)
+            for option in option_set:
+                new_option = Option(
+                    content = option.content,
+                    question = new_question
+                )
+                try:
+                    new_option.save()
+                except ValueError:
+                    raise Http404
+        
         return Response(serializer.data, status=status.HTTP_201_CREATED)
         
